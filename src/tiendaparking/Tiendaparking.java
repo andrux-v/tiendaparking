9449def83f22e5b39aef24a484e1c4d4e56d03bb
andrux-v <andresfvargas22@gmail.com>
2026-08-05 09:03:11 -0500

    Creacion de las vistas con JFrame

--- src/tiendaparking/Tiendaparking.java
+++ src/tiendaparking/Tiendaparking.java
@@ -8,10 +8,9 @@ public class Tiendaparking {
     public static void main(String[] args) {
         vista_general vistaGeneral = new vista_general();
         controlador_general ctrlGeneral = new controlador_general(vistaGeneral);
-
+        
+       
+     
         ctrlGeneral.iniciarSistema();
     }
 }
-
-
-
